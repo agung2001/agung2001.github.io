f7fc7b03203ff8dbbe28cd3c37089fcbde0afc94
Agung Sundoro <muhagung2001@gmail.com>
2022-09-11 20:18:37 +0700

    Finalize grunt and tailwindcss integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,41 @@ module.exports = function(grunt) {
                 `node tailwindcsssupport.js`
             }
         },
+
+        /** CSS Minify */
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    "assets/build/css/tailwind.min.css" : "assets/build/css/tailwind.min.css"
+                }
+            }
+        },
+
+        /** Configure watch task */
+        watch: {
+            css: {
+                files: [
+                    'assets/css/**/*.scss',
+                    'assets/css/**/*.css',
+                    'src/View/**/*.php'
+                ],
+                tasks: ['build-css']
+            },
+        },
     });
 
     /** Load Plugin */
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     /** Register Tasks */
     grunt.registerTask('default', ['watch', 'shell:npm_tailwind']);
-    grunt.registerTask('build', ['shell:npm_tailwind']);
+    grunt.registerTask('build-css', ['shell:npm_tailwind', 'cssmin']);
+    grunt.registerTask('build', ['build-css']);
 
 };
