f013e8c1406700f3abf8ce79c766e69b54c40eba
Agung Sundoro <muhagung2001@gmail.com>
2022-11-26 11:17:15 +0700

    Finish integrating svelte

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,33 @@
+/** Load Javascript Library */
+const path = require("path");
+
+/**
+ * Components Scripts
+ * */
+const components = ['content'];
+const getShellComponents = () => {
+    let shell = {};
+    components.map((c) => {
+        let command = { command: `npx rollup -c assets/components/${c}/rollup.config.js` };
+        shell[`rollup_component_${c}`] = command;
+    });
+    return shell;
+};
+const getWatcherComponents = () => {
+    let watcher = {};
+    components.map((c) => {
+        let FilesTasks = {
+            files: [
+                `assets/components/${c}/**/*.js`,
+                `assets/components/${c}/**/*.svelte`,
+            ],
+            tasks: [`build-component-${c}`, 'build-css']
+        };
+        watcher[`component_${c}`] = FilesTasks;
+    });
+    return watcher;
+};
+
 module.exports = function(grunt) {
 
     /** Configuration */
@@ -6,6 +36,7 @@ module.exports = function(grunt) {
 
         /** Compile TailwindCSS - Cross Platform */
         shell: {
+            ...getShellComponents(),
             npm_tailwind: { command:
                 `npx tailwindcss build assets/css/tailwind/style.css -o assets/build/css/tailwind.min.css --silent && ` +
                 `node tailwindcsssupport.js`
@@ -43,9 +74,17 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+    /** Component */
+    let componentsID = [];
+    components.map((c) => {
+        componentsID.push(`build-component-${c}`);
+        grunt.registerTask(`build-component-${c}`, [ `shell:rollup_component_${c}` ]);
+    });
+
     /** Register Tasks */
     grunt.registerTask('default', ['watch', 'shell:npm_tailwind']);
     grunt.registerTask('build-css', ['shell:npm_tailwind', 'cssmin']);
-    grunt.registerTask('build', ['build-css']);
+    grunt.registerTask('build-js', [ ...componentsID ]);
+    grunt.registerTask('build', ['build-css', 'build-js']);
 
 };
