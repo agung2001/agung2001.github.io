9cad418aa1a3436a6f3f86fe9d4991ce5ada03ad
Agung Sundoro <muhagung2001@gmail.com>
2022-12-16 17:39:56 +0700

    Add Welcome Terminal ASCII message

--- assets/components/content/src/main.js
+++ assets/components/content/src/main.js
@@ -6,6 +6,11 @@ import App from './App.svelte';
 		.then((response) => response.json())
 		.then((config) => {
 			let { name } = config;
+
+			/** Welcome Terminal ASCII */
+			console.log(`༼ つ ◕_◕ ༽つ Welcome to ${name} profile`);
+
+			/** Display Component */
 			document.title = name;
 			const app = new App({
 				target: document.querySelector('#content'),
