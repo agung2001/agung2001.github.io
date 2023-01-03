2f290e4d186fcca166ca2ed54673b1cff7a4dac7
Agung Sundoro <muhagung2001@gmail.com>
2023-01-03 08:11:15 +0700

    chore: revert to minimize version of component

--- assets/components/content/rollup.config.js
+++ assets/components/content/rollup.config.js
@@ -39,7 +39,7 @@ export default {
 		sourcemap: !config.production,
 		format: 'iife',
 		name: 'app',
-		file: 'assets/build/components/content/bundle.js',
+		file: 'assets/build/components/content/bundle.min.js',
 	},
 	plugins: [
 		svelte({
@@ -50,7 +50,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: 'bundle.min.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
