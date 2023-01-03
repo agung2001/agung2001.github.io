e9fab6c2d45c56d96413633e7ab7c9ddbdcc40fe
agung2001 <muhagung2001@gmail.com>
2023-01-03 01:33:59 +0000

    Deploying to gh-pages from @ agung2001/agung2001.github.io@efad0ccddd3ffcb1579a35054c0aed7492d8a19f 🚀

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,101 +3,98 @@
  * */
 const components = ['content']
 const getShellComponents = () => {
-  let shell = {}
-  components.map((c) => {
-    let command = {
-      command: `npx rollup -c assets/components/${c}/rollup.config.js`,
-    }
-    shell[`rollup_component_${c}`] = command
-  })
-  return shell
+	let shell = {}
+	components.map((c) => {
+		let command = {
+			command: `npx rollup -c assets/components/${c}/rollup.config.js`,
+		}
+		shell[`rollup_component_${c}`] = command
+	})
+	return shell
 }
 const getWatcherComponents = () => {
-  let watcher = {}
-  components.map((c) => {
-    let FilesTasks = {
-      files: [
-        `assets/components/${c}/**/*.js`,
-        `assets/components/${c}/**/*.svelte`,
-      ],
-      tasks: [`build-component-${c}`, 'build-css'],
-    }
-    watcher[`component_${c}`] = FilesTasks
-  })
-  return watcher
+	let watcher = {}
+	components.map((c) => {
+		let FilesTasks = {
+			files: [
+				`assets/components/${c}/**/*.js`,
+				`assets/components/${c}/**/*.svelte`,
+			],
+			tasks: [`build-component-${c}`, 'build-css'],
+		}
+		watcher[`component_${c}`] = FilesTasks
+	})
+	return watcher
 }
 
 module.exports = function (grunt) {
-  /** Configuration */
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	/** Configuration */
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    /** Compile TailwindCSS - Cross Platform */
-    shell: {
-      ...getShellComponents(),
-      node_nodesandedges: { command: 'node generator.js' },
-      node_pages: { command: 'node pages.js' },
-      npm_tailwind: {
-        command:
-          `npx tailwindcss build assets/css/tailwind/style.css -o assets/build/css/tailwind.min.css --silent && ` +
-          `node tailwindcsssupport.js`,
-      },
-    },
+		/** Compile TailwindCSS - Cross Platform */
+		shell: {
+			...getShellComponents(),
+			node_nodesandedges: { command: 'node generator.js' },
+			node_pages: { command: 'node pages.js' },
+			npm_tailwind: {
+				command:
+					`npx tailwindcss build assets/css/tailwind/style.css -o assets/build/css/tailwind.min.css --silent && ` +
+					`node tailwindcsssupport.js`,
+			},
+		},
 
-    /** CSS Minify */
-    cssmin: {
-      options: {
-        mergeIntoShorthands: false,
-        roundingPrecision: -1,
-      },
-      target: {
-        files: {
-          'assets/build/css/tailwind.min.css':
-            'assets/build/css/tailwind.min.css',
-        },
-      },
-    },
+		/** CSS Minify */
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1,
+			},
+			target: {
+				files: {
+					'assets/build/css/tailwind.min.css':
+						'assets/build/css/tailwind.min.css',
+				},
+			},
+		},
 
-    /** Configure watch task */
-    watch: {
-      options: {
-        livereload: true,
-      },
-      config: {
-        files: ['config.json'],
-        tasks: ['build'],
-      },
-      css: {
-        files: ['assets/css/**/*.scss', 'assets/css/**/*.css', 'index.html'],
-        tasks: ['build-css'],
-      },
-      ...getWatcherComponents(),
-    },
-  })
+		/** Configure watch task */
+		watch: {
+			options: {
+				livereload: true,
+			},
+			config: {
+				files: ['config.json'],
+				tasks: ['build'],
+			},
+			css: {
+				files: ['assets/css/**/*.scss', 'assets/css/**/*.css', 'index.html'],
+				tasks: ['build-css'],
+			},
+			...getWatcherComponents(),
+		},
+	})
 
-  /** Load Plugin */
-  grunt.loadNpmTasks('grunt-shell')
-  grunt.loadNpmTasks('grunt-contrib-watch')
-  grunt.loadNpmTasks('grunt-contrib-cssmin')
+	/** Load Plugin */
+	grunt.loadNpmTasks('grunt-shell')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-cssmin')
 
-  /** Component */
-  let componentsID = []
-  components.map((c) => {
-    componentsID.push(`build-component-${c}`)
-    grunt.registerTask(`build-component-${c}`, [`shell:rollup_component_${c}`])
-  })
+	/** Component */
+	let componentsID = []
+	components.map((c) => {
+		componentsID.push(`build-component-${c}`)
+		grunt.registerTask(`build-component-${c}`, [`shell:rollup_component_${c}`])
+	})
 
-  /** Register Tasks */
-  grunt.registerTask('build-css', ['shell:npm_tailwind', 'cssmin'])
-  grunt.registerTask('build-js', [...componentsID])
-  grunt.registerTask('build', [
-	  'build-css',
-	  'build-js',
-	  'shell:node_pages'
-  ])
-  grunt.registerTask('default', [
-    'build-css',
-    'build-js',
-    'shell:node_nodesandedges',
-  ])
+	/** Register Tasks */
+	grunt.registerTask('build-css', ['shell:npm_tailwind', 'cssmin'])
+	grunt.registerTask('build-js', [...componentsID])
+	grunt.registerTask('build', ['build-css', 'build-js'])
+	grunt.registerTask('default', [
+		'build-css',
+		'build-js',
+		'shell:node_nodesandedges',
+		'shell:node_pages',
+	])
 }
