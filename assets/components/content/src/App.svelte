2059b36be32f2f8413960f210f7079968e91a1f5
Agung Sundoro <muhagung2001@gmail.com>
2023-01-08 07:45:13 +0700

    fix: add link to readme-stats profile

--- assets/components/content/src/App.svelte
+++ assets/components/content/src/App.svelte
@@ -1,18 +1,20 @@
 <script>
-  export let config
-  import Links from '../sections/Links.svelte'
-  import Profile from '../sections/Profile.svelte'
-  import VisJS from '../sections/VisJS.svelte'
+	export let config
+	import Links from '../sections/Links.svelte'
+	import Profile from '../sections/Profile.svelte'
+	import VisJS from '../sections/VisJS.svelte'
 </script>
 
 <div
-  class="relative sm:mx-auto md:w-3/4 grid sm:grid-rows-1 md:grid-cols-6 gap-4"
+	class="relative sm:mx-auto md:w-3/4 grid sm:grid-rows-1 md:grid-cols-6 gap-4"
 >
-  <div class="bg-white shadow-xl rounded-lg md:col-span-2">
-    <Profile {config} />
-  </div>
-  <div class="md:col-span-4">
-    <VisJS {config} />
-    <Links {config} />
-  </div>
+	<div class="md:col-span-2">
+		<div class="bg-white shadow-xl rounded-lg">
+			<Profile {config} />
+		</div>
+	</div>
+	<div class="md:col-span-4">
+		<VisJS {config} />
+		<Links {config} />
+	</div>
 </div>
