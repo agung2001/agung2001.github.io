e0f836398ede4583aa863e8d8be23ed1824f730c
Agung Sundoro <muhagung2001@gmail.com>
2023-02-18 11:01:28 +0700

    fix: content navigation

--- src/routes/knowledge/components/ContentNavigation.svelte
+++ src/routes/knowledge/components/ContentNavigation.svelte
@@ -1,7 +1,18 @@
 <script>
 	import {onMount} from "svelte";
+	import { fade } from 'svelte/transition';
 	import { requestFullscreen } from "../../../helpers/navigation.ts";
 	import Sticky from "sticky-js"
+
+	/** Navigation State */
+	let navigations = {
+		graph: {
+			visible: false
+		},
+		tableofcontents: {
+			visible: false
+		},
+	}
 
 	/** Lifecycle  onMount */
 	onMount(() => {
@@ -9,20 +20,32 @@
 	})
 </script>
 
-<div id="knowledge-content-navigation" class="flex justify-between p-4 border-b border-gray-200 bg-white">
-	<h2 class="text-lg">Navigation</h2>
-	<div class="flex items-center gap-x-4">
-		<button on:click={() => { window.scrollTo({top: 0, behavior: 'smooth'}); }}>
-			<i class="fas fa-chart-bar"></i>
-		</button>
-		<button on:click={() => { window.scrollTo({top: 0, behavior: 'smooth'}); }}>
-			<i class="fas fa-diagram-project"></i>
-		</button>
-		<button class="hidden md:block" on:click={() => { window.scrollTo({top: 0, behavior: 'smooth'}); }}>
-			<i class="fas fa-arrow-up"></i>
-		</button>
-		<button class="hidden md:block" on:click={() => {requestFullscreen("knowledge-content")}}>
-			<i class="fas fa-expand"></i>
-		</button>
+<div id="knowledge-content-navigation">
+	<div class="flex justify-between p-4 rounded border-b border-gray-200 bg-white">
+		<h2 class="text-lg">Navigation</h2>
+		<div class="flex items-center gap-x-4">
+			<button on:click={() => { navigations.graph.visible = !navigations.graph.visible }}>
+				<i class="fas fa-chart-bar"></i>
+			</button>
+			<button on:click={() => { navigations.tableofcontents.visible = !navigations.tableofcontents.visible }}>
+				<i class="fas fa-diagram-project"></i>
+			</button>
+			<button class="hidden md:block" on:click={() => { window.scrollTo({top: 0, behavior: 'smooth'}); }}>
+				<i class="fas fa-arrow-up"></i>
+			</button>
+			<button class="hidden md:block" on:click={() => {requestFullscreen("knowledge-content")}}>
+				<i class="fas fa-expand"></i>
+			</button>
+		</div>
 	</div>
+	{ #if navigations.graph.visible }
+		<div id="section-graph" class="sections p-4 border-b border-gray-200 bg-white" in:fade out:fade>
+			GRAPH
+		</div>
+	{/if}
+	{ #if navigations.tableofcontents.visible }
+		<div id="section-tableofcontents" class="sections p-4 border-b border-gray-200 bg-white" in:fade out:fade>
+			TABLE OF CONTENTS
+		</div>
+	{/if}
 </div>
--- src/routes/knowledge/components/Knowledge.svelte
+++ src/routes/knowledge/components/Knowledge.svelte
@@ -1,44 +1,8 @@
 <script>
 	import config from "../../../config.js";
 	import github from "../../../github.js";
-	import {onMount} from "svelte";
-	let { name, tagline, codeEnvironment } = config
+	let { name } = config
 	let { username } = github
-
-	onMount(() => {
-		setTimeout(function(){
-			window.addEventListener('DOMContentLoaded', function() {
-				let parentLis = document.querySelectorAll('.tree li:has(ul)');
-				parentLis.forEach(function(parentLi) {
-					parentLi.classList.add('parent_li');
-					parentLi.querySelector(' > span').setAttribute('title', 'Collapse this branch');
-				});
-
-				let parentLiSpans = document.querySelectorAll('.tree li.parent_li > span');
-				parentLiSpans.forEach(function(span) {
-					span.addEventListener('click', function (e) {
-						let children = this.parentElement.querySelector(' > ul > li');
-						if (children.style.display === 'block') {
-							children.style.display = 'none';
-							this.setAttribute('title', 'Expand this branch');
-							let icon = this.querySelector(' > i');
-							icon.classList.add('fa-plus-square');
-							icon.classList.remove('fa-minus-square');
-						} else {
-							children.style.display = 'block';
-							this.setAttribute('title', 'Collapse this branch');
-							let icon = this.querySelector(' > i');
-							icon.classList.add('fa-minus-square');
-							icon.classList.remove('fa-plus-square');
-						}
-						e.stopPropagation();
-					});
-				});
-			});
-
-		}, 500)
-	})
-
 </script>
 
 <div
@@ -51,6 +15,6 @@
 	alt={name}
 />
 <div class="px-6 pt-4 pb-8 sm:px-10">
-	<h1 class="mt-2 text-2xl text-center font-bold">{name}</h1>
-	<p class="mt-2 text-center text-base font-normal text-gray-500">{tagline}</p>
+	<h1 class="mt-2 text-2xl text-center font-bold">Vault</h1>
+	<p class="mt-2 text-center text-base font-normal text-gray-500">README</p>
 </div>
