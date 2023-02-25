199511685a8cfddb3484fbf1effd3c62b5878ffa
Agung Sundoro <muhagung2001@gmail.com>
2023-02-25 07:52:41 +0700

    fix: directory tree to sort by dir then file

--- src/routes/knowledge/components/ListTreeMenu.svelte
+++ src/routes/knowledge/components/ListTreeMenu.svelte
@@ -6,20 +6,34 @@
 
 	const { params } = $page;
 	const { directory } = $Data;
+	const types = ((children) => {
+		if(children.length == 0) return []
+		return {
+			dir: children.filter(child => child.Type == 'dir'),
+			file: children.filter(child => child.Type == 'file')
+		}
+	})(children)
 </script>
 
 {#if children.length}
-	{#each children as child}
-		<li class="py-2 px-4 flex items-center justify-between">
-			{#if child.Type == 'dir'}
-				<span class="caret">{child.Name}</span>
-			{:else}
+	{#if types.dir.length}
+		{#each types.dir as child}
+			<li class="py-2 px-4 flex items-center justify-between">
+				<span class="caret" on:click={() => {
+					console.log(this)
+				}}>{child.Name}</span>
+			</li>
+		{/each}
+	{/if}
+	{#if types.file.length}
+		{#each types.file as child}
+			<li class="py-2 px-4 flex items-center justify-between">
 				<a href="/knowledge/{params.vault}/{child.RelPath}" class="flex items-center">
 					<span>{child.Name}</span>
 				</a>
-			{/if}
-		</li>
-	{/each}
+			</li>
+		{/each}
+	{/if}
 {:else}
 	{#if directory.length}
 		<ListTreeMenu children={directory[0].Children} />
