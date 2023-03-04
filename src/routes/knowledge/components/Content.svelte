1545bfd4b013332f4e7ae7b62ad2eb840cc441ad
Agung Sundoro <muhagung2001@gmail.com>
2023-03-04 22:42:33 +0700

    feat: add support for mermaid js

--- src/routes/knowledge/components/Content.svelte
+++ src/routes/knowledge/components/Content.svelte
@@ -1,5 +1,15 @@
 <script>
+	import mermaid from "mermaid";
+	import {onMount} from "svelte";
 	export let content = '';
+
+	/** Lifecylce onMount */
+	onMount(async () => {
+		mermaid.initialize({ startOnLoad: false });
+		await mermaid.run({
+			querySelector: '.language-mermaid',
+		});
+	})
 </script>
 
 <div id="knowledge-content" class="p-6">
