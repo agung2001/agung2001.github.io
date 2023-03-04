bd6340ddaac6e1530bab3c60680953725762d3c5
Agung Sundoro <muhagung2001@gmail.com>
2023-03-04 19:07:13 +0700

    fix: vaults-sample.json remove unecessary setting

--- src/routes/knowledge/components/Knowledge.svelte
+++ src/routes/knowledge/components/Knowledge.svelte
@@ -10,16 +10,12 @@
 </script>
 
 <div class="bg-white">
-	{#if vault.assets.cover}
-		<div class="h-44 bg-cover bg-center bg-no-repeat" style="{cover}" />
-	{/if}
-	{#if vault.assets.logo}
-		<img
-			src={`/vaults/${params.vault}/logo.jpg`}
-			class="w-44 h-44 -mt-32 mx-auto ring-4 ring-white shadow-xl rounded-full z-99"
-			alt={name}
-		/>
-	{/if}
+	<div class="h-44 bg-cover bg-center bg-no-repeat" style="{cover}" />
+	<img
+		src={`/vaults/${params.vault}/logo.jpg`}
+		class="w-44 h-44 -mt-32 mx-auto ring-4 ring-white shadow-xl rounded-full z-99"
+		alt={name}
+	/>
 	<div class="px-6 pt-4 pb-4 sm:px-10">
 		<h1 class="mt-2 text-2xl text-center font-bold">{name}</h1>
 		<p class="mt-2 text-center text-base font-normal text-gray-500">{ capitalizeFirstLetter(params.vault) } Knowledge</p>
