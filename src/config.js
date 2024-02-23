9a788eb5ccd8f91db28bf38e24525d91cf338ad2
Agung Sundoro <muhagung2001@gmail.com>
2024-02-24 06:56:01 +0700

    fix: broken icons

--- src/config.js
+++ src/config.js
@@ -7,75 +7,75 @@ export default {
 		{
 			"label": "Languages",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/markdown/markdown-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg"
 			]
 		},
 		{
 			"label": "Frontend",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
 			]
 		},
 		{
 			"label": "Framework",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/woocommerce/woocommerce-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cakephp/cakephp-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/codeigniter/codeigniter-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/woocommerce/woocommerce-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cakephp/cakephp-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/codeigniter/codeigniter-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
 			]
 		},
 		{
 			"label": "Database",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg"
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redis/redis-original.svg"
 			]
 		},
 		{
 			"label": "Tools",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/subversion/subversion-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/filezilla/filezilla-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/grunt/grunt-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg"
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jetbrains/jetbrains-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vim/vim-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/subversion/subversion-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/filezilla/filezilla-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/grunt/grunt-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/yarn/yarn-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/selenium/selenium-original.svg"
 			]
 		},
 		{
 			"label": "Infrastructure",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/digitalocean/digitalocean-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/digitalocean/digitalocean-original.svg",
 				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
 				"https://cdn.worldvectorlogo.com/logos/aws-ec2.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/heroku/heroku-plain.svg",
 				"https://cdn.worldvectorlogo.com/logos/rabbitmq.svg",
 				"https://cdn.worldvectorlogo.com/logos/elasticsearch.svg",
 				"https://cdn.worldvectorlogo.com/logos/teamcity-icon.svg"
@@ -84,11 +84,11 @@ export default {
 		{
 			"label": "Team",
 			"tools": [
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/trello/trello-plain.svg",
 				"https://cdn.worldvectorlogo.com/logos/yourtrack.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/confluence/confluence-original.svg",
-				"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/confluence/confluence-original.svg",
+				"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg",
 				"https://cdn.worldvectorlogo.com/logos/teamwork-2.svg"
 			]
 		}
