d1e46fc2043b485a9ca121a40f91470506c6ffd2
Agung Sundoro <muhagung2001@gmail.com>
2024-05-27 07:59:58 +0700

    fix: restructure social links

--- src/socials.js
+++ src/socials.js
@@ -118,6 +118,39 @@ export default [
 		]
 	},
 	{
+		"label": "Contribution",
+		"buttons": [
+			{
+				"label": "Github - Syncthing",
+				"caption": "Open Source File Synchronization",
+				"link": "https://github.com/syncthing",
+				"icon": "fab fa-github",
+				"color": "#24292f"
+			},
+			{
+				"label": "Ko-Fi - Keep-Presence",
+				"caption": "A simple library to keep your app awake",
+				"link": "https://ko-fi.com/agungsundoro",
+				"icon": "fa-solid fa-mug-saucer",
+				"color": "#ff5f5f"
+			},
+			{
+				"label": "Open Collective - OpenSID",
+				"caption": "Open Source Information System for Government",
+				"link": "https://opencollective.com/opensid",
+				"icon": "fas fa-circle-notch",
+				"color": "#97b3e7"
+			},
+			{
+				"label": "Open Collective - Svelte",
+				"caption": "Cybernetically enhanced web apps",
+				"link": "https://opencollective.com/svelte",
+				"icon": "fas fa-circle-notch",
+				"color": "#97b3e7"
+			}
+		]
+	},
+	{
 		"label": "Contact",
 		"buttons": [
 			{
@@ -163,37 +196,4 @@ export default [
 			},
 		]
 	},
-	{
-		"label": "Contribution",
-		"buttons": [
-			{
-				"label": "Github - Syncthing",
-				"caption": "Open Source File Synchronization",
-				"link": "https://github.com/syncthing",
-				"icon": "fab fa-github",
-				"color": "#24292f"
-			},
-			{
-				"label": "Ko-Fi - Keep-Presence",
-				"caption": "A simple library to keep your app awake",
-				"link": "https://ko-fi.com/agungsundoro",
-				"icon": "fa-solid fa-mug-saucer",
-				"color": "#ff5f5f"
-			},
-			{
-				"label": "Open Collective - OpenSID",
-				"caption": "Open Source Information System for Government",
-				"link": "https://opencollective.com/opensid",
-				"icon": "fas fa-circle-notch",
-				"color": "#97b3e7"
-			},
-			{
-				"label": "Open Collective - Svelte",
-				"caption": "Cybernetically enhanced web apps",
-				"link": "https://opencollective.com/svelte",
-				"icon": "fas fa-circle-notch",
-				"color": "#97b3e7"
-			}
-		]
-	}
 ]
