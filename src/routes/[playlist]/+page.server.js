dbc1b51c1d296c96e1a60944f8562dec230649de
Agung Sundoro <muhagung2001@gmail.com>
2024-09-02 18:22:34 +0700

    feat: initial setup playlist page

---
+++ src/routes/[playlist]/+page.server.js
@@ -0,0 +1,17 @@
+import { error } from '@sveltejs/kit';
+import youtube from '../../youtube.js'
+import {removeEmojisAndSymbols} from "$lib/strings.ts";
+import {slugify} from "$lib/url.ts";
+
+// @ts-ignore
+export async function load({ params }) {
+	// Check if playlist exists
+	let exist = youtube.find((p) => {
+		let slug = removeEmojisAndSymbols(p.name).trimStart()
+		slug = slugify(slug)
+		return params.playlist === slug;
+	});
+
+	// Throw error if not
+	if(!exist) throw error(404, { message: 'Not Found' });
+}
