7252115c468829d007cdbe77c0f638f3bcd3dda8
Agung Sundoro <muhagung2001@gmail.com>
2024-09-02 14:26:58 +0700

    feat: finish integrate flowbite

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,32 +1,20 @@
 <script>
+	import { page } from '$app/stores';
 	import config from "../../config.js";
-
-	let isMenuOpen = false;
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	$: activeUrl = $page.url.pathname;
 </script>
 
-<nav id="main-nav" class="top-0 px-6 py-4 w-full bg-white border-b border-gray-300 relative z-50">
-	<div class="container mx-auto flex justify-between items-center">
-		<!-- Logo -->
-		<a href="/" class="text-xl">
-			😎 <span class="pl-2">{config.name}</span>
-		</a>
-
-		<!-- Menu Button for Mobile -->
-		<button on:click={() => { isMenuOpen = !isMenuOpen; }} class="md:hidden focus:outline-none">
-			<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-			</svg>
-		</button>
-
-		<!-- Links -->
-		<div class="md:flex md:space-x-6"
-			 class:responsive={isMenuOpen}
-			 class:divide-y={isMenuOpen}
-			 class:hidden={!isMenuOpen}
-		>
-			<a href="/knowledge">🧠 Knowledge</a>
-			<a href="/contribution">🌟 Contribution</a>
-			<a href="https://store.agungsundoro.com">🛒 Store</a>
-		</div>
-	</div>
-</nav>
+<Navbar class="top-0 px-4 w-full bg-white border-b border-gray-300 relative z-50">
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl dark:text-white">
+			😎 {config.name}
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl} classUl="pr-0" activeClass="text-amber-500">
+		<NavLi href="/knowledge">Knowledge</NavLi>
+		<NavLi href="/contribution">Contribution</NavLi>
+		<NavLi href="https://store.agungsundoro.com">Store</NavLi>
+	</NavUl>
+</Navbar>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 module.exports = {
   mode: 'jit',
-  content: [ "./src/**/*.{html,js,svelte,ts}" ],
+  content: [
+	  "./src/**/*.{html,js,svelte,ts}",
+	  './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', // Include Flowbite Svelte components
+  ],
   theme: {
     extend: {},
   },
   plugins: [
 	  require('@tailwindcss/typography'),
+	  require('flowbite/plugin'), // Add Flowbite as a plugin
   ],
   darkMode: 'class',
 }
