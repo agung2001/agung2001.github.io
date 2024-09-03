115a5d470806c95cded924f938eb4528cf584d84
Agung Sundoro <muhagung2001@gmail.com>
2024-09-03 09:04:20 +0700

    fix: episode redirection incorrect index

--- src/routes/[playlist]/[episode]/+page.server.js
+++ src/routes/[playlist]/[episode]/+page.server.js
@@ -13,9 +13,10 @@ export async function load({ params }) {
 		return params.playlist === slug;
 	});
 
-	// Throw error if not
-	if(!playlist || typeof playlist.videos[params.episode] === 'undefined') throw error(404, { message: 'Not Found' });
+	// Throw error if not exist
+	if(!playlist || typeof playlist.videos[params.episode-1] === 'undefined') throw error(404, { message: 'Not Found' });
+	let video = playlist.videos[params.episode-1]
 
 	// Redirect to YouTube
-	throw redirect(302, playlist.videos[params.episode].url);
+	throw redirect(302, video.url);
 }
